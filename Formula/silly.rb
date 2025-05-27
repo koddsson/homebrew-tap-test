0ca88ffcca789b531838962b71573f1d88c75749
Kristján Oddsson <koddsson@gmail.com>
2025-05-27 15:30:07 +0000

    update sha256

--- Formula/silly.rb
+++ Formula/silly.rb
@@ -1,9 +1,12 @@
+# frozen_string_literal: true
+
+# Just a silly little Formula
 class Silly < Formula
   desc 'Just a silly little Formula'
   homepage 'https://github.com/koddsson/homebrew-tap-test'
   url "file://#{Pathname(__FILE__).dirname.parent}/script/tap-test"
   version '1'
-  sha256 '46fd63013c5a7855d0fbe9f9e95e28ddf977cdf80f24faf4e23782a2e49d5aab'
+  sha256 '05c6ae5ef8dd50baaad0b1907e35c8267b4dbcd2539f4ec32da999b8507988cd'
   license 'BSD-3-Clause'
 
   def install
